691007b03ae934d49665b0b801e6b1af255d4613
doogler <doogler@google.com>
2023-04-04 08:05:41 +0000

    (Mtc) Convert Call State of JniExternalCall for Telephony interface
    This commit is to convert the m_nCallState value of JniExternalCall to
    be aligned with ImsExternalCallState in Telephony framework. Also, this
    changes MultiEndpointManager not to notify to telephony if the call
    state is not yet CALL_STATE_CONFIRMED.
    
    Bug: 224439978
    Test: atest ImsStackNativeEnablerMtcTests
    Test: verified with test code and dialer
    Change-Id: I108c46b2b47fd69a71dd24edb6f6262aa28a6d64

--- native/libimsstack/enabler/mtc/dialogevent/MultiEndpointManager.cpp
+++ native/libimsstack/enabler/mtc/dialogevent/MultiEndpointManager.cpp
@@ -110,7 +110,6 @@ VIRTUAL PUBLIC IMultiEndpointManager::PullingDialogInfo MultiEndpointManager::Ge
     {
         const Dialog* pDialog = m_piDialogInfoManager->GetDialogs().GetAt(i);
 
-        // TODO: This will be determined by the information passed by Telephony layer.
         AString strRemoteNumber = pDialog->GetRemoteParticipant().GetIdentity().GetUri();
         IMS_TRACE_I("GetDialogInfo RemoteNumber[%s]", strRemoteNumber.GetStr(), 0, 0);
         if (strRemoteNumber.Contains(strTarget))
@@ -292,6 +291,13 @@ ImsList<const JniExternalCall*> MultiEndpointManager::GetJniExternalCalls() cons
             continue;
         }
 
+        if (IsEarlyState(objDialog))
+        {
+            IMS_TRACE_D("GetJniExternalCalls ignores early states [%d]",
+                    objDialog.GetState().GetState(), 0, 0);
+            continue;
+        }
+
         JniExternalCall* pJniExternalCall = new JniExternalCall();
 
         pJniExternalCall->m_strCallId = objDialog.GetId();
@@ -299,13 +305,14 @@ ImsList<const JniExternalCall*> MultiEndpointManager::GetJniExternalCalls() cons
         pJniExternalCall->m_strLocalAddress =
                 objDialog.GetLocalParticipant().GetIdentity().GetUri();
         pJniExternalCall->m_bIsPullable = IsPullable(objDialog);
-        pJniExternalCall->m_nCallState = objDialog.GetState().GetState();
+        pJniExternalCall->m_nCallState =
+                objDialog.GetState().GetState() == Dialog::State::STATE_CONFIRMED
+                ? JniExternalCall::CALL_STATE_CONFIRMED
+                : JniExternalCall::CALL_STATE_TERMINATED;
         pJniExternalCall->m_nCallType = static_cast<IMS_UINT32>(GetCallType(objDialog));
         pJniExternalCall->m_bIsHeld = IsHeld(objDialog);
 
-        IMS_TRACE_D("GetJniExternalCalls CallId[%s] Address[%s] LocalAddress[%s]",
-                pJniExternalCall->m_strCallId.GetStr(), pJniExternalCall->m_strAddress.GetStr(),
-                pJniExternalCall->m_strLocalAddress.GetStr());
+        IMS_TRACE_D("GetJniExternalCalls >> %s", pJniExternalCall->ToString().GetStr(), 0, 0);
 
         objJniExternalCalls.Append(pJniExternalCall);
     }
@@ -398,4 +405,19 @@ IMS_BOOL MultiEndpointManager::IsOwnDialog(const Dialog& objDialog) const
     }
 
     return IMS_FALSE;
+}
+
+PRIVATE
+IMS_BOOL MultiEndpointManager::IsEarlyState(const Dialog& objDialog) const
+{
+    switch (objDialog.GetState().GetState())
+    {
+        case Dialog::State::STATE_IDLE:
+        case Dialog::State::STATE_TRYING:
+        case Dialog::State::STATE_PROCEEDING:
+        case Dialog::State::STATE_EARLY:
+            return IMS_TRUE;
+        default:
+            return IMS_FALSE;
+    }
 }
